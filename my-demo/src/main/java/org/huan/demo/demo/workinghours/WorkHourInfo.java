ad3d14b536b3f67b4780a143cd31467d823da385
zhanghuan <huan.zhang@bafangseis.com>
2026-07-22 15:25:57 +0800

    init project

--- my-demo/src/main/java/org/huan/demo/demo/workinghours/WorkHourInfo.java
+++ my-demo/src/main/java/org/huan/demo/demo/workinghours/WorkHourInfo.java
@@ -39,9 +39,9 @@ public class WorkHourInfo {
 
     private int totalWorkHour = 0;
 
-    public static int PERSON_NAME_INDEX = 10;
-    public static int PROJECT_NAME_INDEX = 8;
-    public static int WORK_HOUR_INDEX = 19;
+    public static String PERSON_NAME = "K";
+    public static String PROJECT_NAME = "I";
+    public static String WORK_HOUR = "U";
 
 
 
@@ -259,9 +259,9 @@ public class WorkHourInfo {
         List<Object> objects = EasyExcel.read(excelPath, new AnalysisEventListener<Map<Integer, String>>() {
             @Override
             public void invoke(Map<Integer, String> data, AnalysisContext context) {
-                String name = data.get(PERSON_NAME_INDEX);
-                String projectName = data.get(PROJECT_NAME_INDEX);
-                String workHourPercent = data.get(WORK_HOUR_INDEX);
+                String name = data.get(columnIndex(PERSON_NAME));
+                String projectName = data.get(columnIndex(PROJECT_NAME));
+                String workHourPercent = data.get(columnIndex(WORK_HOUR));
                 if (StrUtil.isBlank(name) || StrUtil.isBlank(workHourPercent)) {
                     return;
                 }
@@ -306,10 +306,22 @@ public class WorkHourInfo {
         totalWorkHour = (int) count * DAY_HOURS;
     }
 
+    /**
+     * 根据excel列名获取对应下标 从0开始
+     */
+    private int columnIndex(String columnName) {
+        int result = 0;
+        for (int i = 0; i < columnName.length(); i++) {
+            char c = Character.toUpperCase(columnName.charAt(i));
+            result = result * 26 + (c - 'A' + 1);
+        }
+        return result - 1;
+    }
+
     public static void main(String[] args) throws IOException {
         String path = "D:\\个人\\temp\\2026研发人员分配.xlsx";
-        WorkHourInfo workHourInfo = new WorkHourInfo(path, 6, 0);
-        workHourInfo.addHolidays(19);
+        WorkHourInfo workHourInfo = new WorkHourInfo(path, 7, 0);
+//        workHourInfo.addHolidays(19);
         workHourInfo.writeExcel("D:\\个人\\temp\\2026研发人员分配-完成.xlsx");
     }
 
